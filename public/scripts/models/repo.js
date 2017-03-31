201b33518b7c6be7ce13b0669e109ad0a06af67f
Anna Shelby <bonanashelby@gmail.com>
2017-03-31 10:55:12 -0700

    changed heroku token

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -10,7 +10,7 @@
     $.ajax({
       url: `https://api.github.com/user/repos`,
       type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
+      headers: {'Authorization': `token ${process.env.GITHUB_TOKEN}`}
     })
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
